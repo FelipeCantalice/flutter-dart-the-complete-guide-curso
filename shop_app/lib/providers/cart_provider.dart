a1fac692e0550a7b69ee6a23fb34388ee10e6a79
Felipe <felipep31a@gmail.com>
2021-05-22 16:52:47 -0300

    Card provider

---
+++ shop_app/lib/providers/cart_provider.dart
@@ -0,0 +1,42 @@
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/cart_item.dart';
+import 'package:shop_app/models/product.dart';
+
+class CartProvider with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items => {..._items};
+
+  int get itemsCount => items.length;
+
+  double get itemsCartTotal {
+    double total = 0.00;
+    _items.forEach((_, item) {
+      total += item.getTotal();
+    });
+    return total;
+  }
+
+  void addItem(Product product) {
+    // incrementar a quantidade
+    if (_items.containsKey(product.id)) {
+      _items.update(product.id, (CartItem existingProduct) {
+        existingProduct.incrementQuantity();
+        return existingProduct;
+      });
+      notifyListeners();
+      return;
+    }
+    // adicionar um item para o carrinho
+    _items.putIfAbsent(product.id, () => CartItem.fromProduct(product));
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    print(_items);
+    _items.remove(productId);
+    notifyListeners();
+  }
+}
